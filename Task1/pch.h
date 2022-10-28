a27d09060152938ae548724c271718e8e96da6cb
VirtualJabberWock <loopinyou12@gmail.com>
2022-10-28 10:02:53 +0500

    update2

--- Task1/pch.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "DynamicList.h"
-#include "DebugUtils.h"
-#include "FileUtils.h"
-#include "StringUtils.h"
--- Task2/aquaLib/pch.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "DynamicList.h"
-#include "DebugUtils.h"
-#include "FileUtils.h"
-#include "StringUtils.h"
-#include "Storage.h"
